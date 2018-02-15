c8dc27eb1b2345fd5f388e51ffb2d2e4fb2e3f59
Violeta Mezeklieva <35139125+vmez@users.noreply.github.com>
2018-02-15 17:11:48 +0100

    Fixing plots + Reviewing Concepts

--- MBAcode.R
+++ MBAcode.R
@@ -1,33 +1,49 @@
-electronidex<-read.transactions("C:/Users/Violeta/Desktop/Ubiqum/PredictingCustomerPreference/Electronidex/ElectronidexTransactions2017.csv", 
-                                format="basket", sep = ",",
-                                cols = NULL, rm.duplicates = TRUE, encoding = "unknown")
-itemLabels(electronidex)
+#Start with Clean Environment
+rm(list = ls())
 
+#Libraries---------------------------------------
+library(readr)
+library(arules)
+library(Matrix)
+library(lattice)
+library(ggplot2)
+library(caret)
+library(grid)
+library(arulesViz)
 
+#Load data---------------------------------------
+
+electronidex <- read.transactions(
+  "Ubiqum/PredictingCustomerPreference/Electronidex/ElectronidexTransactions2017.csv",
+  format = "basket", sep = ",", cols = NULL, rm.duplicates = TRUE, encoding = "unknown")
+
+Electrodinex_itemList
+
+#What's here------------------------------------
 inspect(electronidex[1:10])
+length(electronidex)
+LIST(electronidex)
+length(unique(itemLabels(electronidex)))
+
 summary(electronidex)
-M1<- apriori (electronidex, parameter=list(support=0.007, confidence=0.60, minlen=2))
-summary(M1)
-inspect(sort(M1, by="lift")[1:8])
-
-M<-matrix(itemFrequency(electronidex, type="abs"))
 
 
-itemFrequencyPlot(electronidex, support=0.075)
-itemFrequencyPlot(electronidex, TopN=10)
-itemFrequency(electronidex[, 1:10])
+#Let's Plot for clarity-------------------------
+itemFrequencyPlot(electronidex, topN = 20, horiz = TRUE, type = "absolute")
+
+
+#PreProcessing----------------------------------
 rules <- apriori(electronidex, parameter = list(support = 0.002, confidence = 0.25, minlen = 2))
 inspect(sort(rules, by = 'count')[1:10])
 
-#how manay times is an item purchased alone
-size(electronidex)
+#how manay times is an item purchased alone----------------------------
 table1<-electronidex[which(size(electronidex)==1),]
 size(table1)
 tbl<-crossTable(table1)
 tbl["iMac", "iMac"]
 
 
-#######see redundant
+#redundant purchases------------------------------------------------
 subset.matrix <- is.subset(rules, rules)
 subset.matrix[lower.tri(subset.matrix, diag=T)] <- TRUE 
 redundant <- colSums(subset.matrix, na.rm=T) >= 1
@@ -37,8 +53,8 @@ rules.pruned <- rules[!redundant]
 inspect(rules.pruned)
 
 iMac.rules <- subset(electronidex, items %in% 'iMac')
-inspect(ham.rules)
-#############################################################################################################
+inspect(iMac.rules)
+image(iMac.rules)
 inspect(rules[1:length(rules)])
 plot(rules)
 #plot(rules, method = NULL, measure = "support", shading = "lift",
@@ -46,52 +62,34 @@ plot(rules)
 head(quality(rules))
 sel <- plot(rules, measure=c("support", "lift"), shading="confidence", interactive=TRUE)
 
-
-
-
-
 #############################################################################################################
-itemFrequencyPlot(electronidex,topN=10,type="absolute")
-
 ########apriori recommendation
 rules<-apriori(electronidex, parameter = list(supp = 0.05, conf = 0.01))
 inspect(rules[1:length(rules)])
-#inspect(rules[1:4])
+inspect(rules[1:4])
 summary(rules)
 
 
-
-########plotting the volume with categories combined
-agsuM1$Group.1 <- factor(agsuM1$Group.1, levels = agsuM1$Group.1[order(-agsuM1$`Electronidex_itemsList$volume`)]) 
-agsuM1$Group.1
-p<-ggplot(data=agsuM1, aes(x=as.factor(agsuM1$Group.1), y=agsuM1$`Electronidex_itemsList$volume`))+
-  geom_bar(stat="identity")+labs(title="Frequency Product Category")
-p + theme(axis.text.x = element_text(angle= 70, hjust = 1)) + ylab("Volume") +xlab("Product Category") +
-  theme(plot.title = element_text(hjust = 0.5))
-summary(agsuM1)
-
-
-
-#Product dependency on rhs as Apple Earpods
+#Product dependency on rhs as Apple Earpods-------------------------------------------------
 rulesEarPods<-apriori(electronidex, parameter=list(supp=0.001,conf = 0.6, minlen=2), 
                       appearance = list(rhs="Apple Earpods"))
 
 
 plot(rulesEarPods, method="graph")
 
-#Product dependency on lhs as Apple Earpods
+#Product dependency on lhs as Apple Earpods--------------------------------------------------
 rulesEarPods<-apriori(electronidex, parameter=list(supp=0.001,conf = 0.1, minlen=2), 
                       appearance = list(lhs="Apple Earpods"))
 rulesEarPods<-sort(rulesEarPods, decreasing=TRUE,by="lift")
 inspect(rulesEarPods)
 
-#3-button-mouse dependency
+#3-button-mouse dependency--------------------------------------------------------------------
 rulesMouse<- apriori(electronidex, parameter = list(supp=0.002, conf=0.3, minlen=2),
                      appearance = list(rhs="3-Button Mouse"))
 rulesMouse<-sort(rulesMouse, decreasing = TRUE, by="lift")
 inspect(rulesMouse)
 
-##looking at high product relationship
+##looking at high product relationship--------------------------------------------------------
 rules<-apriori(electronidex, parameter = list(supp=0.009, conf=0.08, minlen=2))
 subset.matrix <- is.subset(rules, rules)
 subset.matrix[lower.tri(subset.matrix, diag=T)] <- TRUE 
@@ -102,3 +100,72 @@ rules.pruned <- rules[!redundant]
 inspect(rules.pruned)
 rules<-sort(rules, decreasing = TRUE, by="lift")
 inspect(rules[1:10])
+
+
+#List of product upload---------------------------------------------------------------------------
+Electronidex_itemsList <- read_csv("Ubiqum/PredictingCustomerPreference/Electronidex/Electronidex_itemsList.csv")
+str(Electronidex_itemsList)
+Electronidex_itemsList <- as.data.frame(Electronidex_itemsList)
+head(Electronidex_itemsList)
+
+
+#Volume Database----------------------------------------------------------------------------------
+agsum <- as.data.frame(Electronidex_itemsList$frequency)
+str(agsum)
+agsuM<-aggregate(agsum, by=list(Electronidex_itemsList$category), FUN= sum) 
+View(agsuM)
+
+
+#Plotting the volume with categories combined--------------------------------------------
+agsuM$Group.1 <- factor(agsuM$Group.1, 
+                        levels = agsuM$Group.1[order(-agsuM$`Electronidex_itemsList$volume`)]) 
+
+p<-ggplot(agsuM1, aes(as.factor(agsuM$Group.1), agsuM$`Electronidex_itemsList$volume`))+
+  geom_bar(stat="identity")+labs(title="Frequency Product Category")
+
+p + theme(axis.text.x = element_text(angle= 70, hjust = 1)) + 
+  ylab("Volume") +xlab("Product Category") +
+  theme(plot.title = element_text(hjust = 0.5))
+summary(agsuM)
+
+
+#Viz Products top3---------------------------------------------------
+top3Cat <- read_csv("Ubiqum/PredictingCustomerPreference/Electronidex/top3Cat.csv")
+
+
+#when three categories combined in one plot (desktop, laptop, monitor)-----------------------
+ggplot(top3Cat) +
+  geom_bar(aes(type, volume, fill = category), stat="identity")+
+  facet_wrap(~ category, drop = TRUE)+
+  theme(axis.text.x = element_text(angle= 70, hjust = 1))
+
+
+#when ploting individual categories (desktop, laptop, monitor)---------------------------------
+ggplot(top3Cat[which(top3Cat$category == "Desktop"), ]) +
+  geom_bar(aes(type, volume, fill = category), stat="identity")+
+  theme(axis.text.x = element_text(angle= 70, hjust = 1))+ 
+  labs(title="Desktop Products", ylab("Volume"), xlab("Product Type"))
+
+
+#plotting in order. first, order the daa set---------------------------------------------------
+top3Cat$type<- factor(top3Cat$type, levels=top3Cat$type[order(-top3Cat$volume)])
+
+ggplot(top3Cat[which(top3Cat$category == "Desktop"), ]) +
+  geom_bar(aes(type, volume, fill = category), stat="identity")+
+  theme(axis.text.x = element_text(angle= 25, hjust = 1))+ 
+  labs(title="Desktop Products", ylab("Volume"), xlab("Product Type"))
+
+
+#plotting Laptops------------------------------------------------------------------------------
+ggplot(top3Cat[which(top3Cat$category == "Laptops"), ]) +
+  geom_bar(aes(x = type, y=volume, fill = category),fill="orange", stat="identity")+
+  theme(axis.text.x = element_text(angle= 25, hjust = 1))+ 
+  labs(title="Laptop Products", ylab("Volume"), xlab("Product Type"))
+
+
+#plotting Monitor------------------------------------------------------------------------------
+ggplot(top3Cat[which(top3Cat$category=="Monitors"),])+
+  geom_bar(aes(type, volume, fill=category), fill="purple", stat = "identity")+
+  theme(axis.text.x = element_text(angle = 25,hjust = 1))+
+  labs(title="Monitor Products", ylab("Volume"), xlab("Product Type"))
+
